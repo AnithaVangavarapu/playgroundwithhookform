b3190a0e566018009c3b42d746dbcd61af3ec7f0
AnithaVangavarapu <anitha.vangavarapu@quadone.com>
2025-05-16 18:31:35 +0530

    rule conversion is in progress

--- src/utils/ruleConversion.ts
+++ src/utils/ruleConversion.ts
@@ -15,11 +15,33 @@ export const ruleConversion = (validation: Validation) => {
       if (key === "lessThan") {
         if (value.value === "today") {
           const currentDate = new Date();
+
           rules.lessThan = {
             value: currentDate,
             message: value.message,
           };
         }
+        if (value.value === "now") {
+          const currentDate = new Date();
+          const currentTime = currentDate.toLocaleTimeString;
+          rules.lessThan = {
+            value: currentTime,
+            message: value.message,
+          };
+        }
+      }
+      if (key === "pattern") {
+        rules.pattern = {
+          value: value.pattern,
+          message: value.message,
+        };
+      }
+      if (key === "range") {
+        rules.range = {
+          min: value.min,
+          max: value.max,
+          message: value.message,
+        };
       }
     });
   }
